aadb020bb12d7c22800a723b1a5b2e9da8242396
greygn <78500030+greygn@users.noreply.github.com>
2025-05-19 22:52:32 +0400

    imports

--- forum-service/internal/service/grpc.go
+++ forum-service/internal/service/grpc.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/greygn/protos/gen/go/proto/forum"
+	"github.com/greygn/forum-service/protos/gen/go/proto/forum"
 
 	"github.com/google/uuid"
 )
